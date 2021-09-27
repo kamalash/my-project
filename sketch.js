decd74a06dc4231099ee8f3b43c51e32f8439580
kamalash <91454796+kamalash@users.noreply.github.com>
2021-09-27 12:54:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,20,20)
+  box.shapeColor = "black";
+}
+
+function draw() 
+{
+  background("red");
+
+  drawSprites();
+}
+
+
+
+
